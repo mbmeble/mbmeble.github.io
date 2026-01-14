e359dc5260ddf34fa610cf15c66756ae249aeeb8
mbmeble <mbsklepmeblowy@gmail.com>
2026-01-15 00:02:31 +0100

    Add files via upload

---
+++ kuchnia2b/data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-w360-1",
+      "name": "w360-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.27971240994521,
+          "pitch": 0.31337623060481334,
+          "rotation": 0,
+          "target": "1-w360-2a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-w360-2a",
+      "name": "w360-2a",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2171552016317797,
+          "pitch": 0.2238021467423046,
+          "rotation": 0,
+          "target": "0-w360-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "kuchnia2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
