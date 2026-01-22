3a1c0e359519342ffe0cca65cb3dd160fa4d56bd
mbmeble <mbsklepmeblowy@gmail.com>
2026-01-22 18:24:38 +0100

    Add files via upload

---
+++ ania/data.js
@@ -0,0 +1,195 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ramki1",
+      "name": "ramki1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23198075570900478,
+          "pitch": -0.01921755480159959,
+          "rotation": 0,
+          "target": "1-ramki1plyta"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0758050476706007,
+          "pitch": 0.3746506301903558,
+          "title": "Blat 2738 FP",
+          "text": "<h1 class=\"page-header \">Dąb Cremona Torro</h1>"
+        }
+      ]
+    },
+    {
+      "id": "1-ramki1plyta",
+      "name": "ramki1plyta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2276073682787576,
+          "pitch": -0.02044279445679109,
+          "rotation": 0,
+          "target": "2-dol-drewno"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dol-drewno",
+      "name": "dol-drewno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2307839570923349,
+          "pitch": -0.02369058863695983,
+          "rotation": 0,
+          "target": "3-gora-drewno"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.22519540430505813,
+          "pitch": 0.39712584231117454,
+          "title": "<h2 class=\"deco-title\">blat K209&nbsp;</h2>",
+          "text": "<h2 class=\"deco-title\">Crema Limestone\n</h2>"
+        }
+      ]
+    },
+    {
+      "id": "3-gora-drewno",
+      "name": "gora-drewno",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22897747668707602,
+          "pitch": -0.027153519524858893,
+          "rotation": 0,
+          "target": "4-otwarte"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-otwarte",
+      "name": "otwarte",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2289774484201903,
+          "pitch": -0.01939771003509705,
+          "rotation": 0,
+          "target": "0-ramki1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Ania",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
