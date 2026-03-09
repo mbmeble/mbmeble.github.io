1f60f18c6b0622ca482a48ecef53358841261d85
mbmeble <mbsklepmeblowy@gmail.com>
2026-03-09 16:48:34 +0100

    Add files via upload

---
+++ lewy360/data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lewy360",
+      "name": "lewy360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8210945432907,
+          "pitch": -0.12309703629962598,
+          "rotation": 0,
+          "target": "1-lewy360wnetrz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-lewy360wnetrz",
+      "name": "lewy360wnetrz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8204317188219221,
+          "pitch": -0.11460734866409084,
+          "rotation": 0,
+          "target": "0-lewy360"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "prawy360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
