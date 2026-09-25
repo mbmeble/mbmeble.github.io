4755f8e0b8972bc928eee33a3992228526608dcd
mbmeble <mbsklepmeblowy@gmail.com>
2026-09-25 10:08:36 +0200

    v2

---
+++ wcv2/data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-wcv2",
+      "name": "wcV2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "wcV2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
